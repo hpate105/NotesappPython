9492f281f7006c2e472d44772c6efe2d0c76a969
Harsh Patel <hpate105@uncc.edu>
2021-03-02 18:31:36 -0500

    Just adding few things

--- PythonBasics1/pythonBasics1.py
+++ PythonBasics1/pythonBasics1.py
@@ -8,19 +8,33 @@
 # Define a function count_char(s, char) that takes a string and a character
 # and returns the number of times the given character appears in the string
 def count_char(s, char):
-  # YOUR CODE HERE
+    count = 0
 
-  return
+    for i in s:
+        if i == char:
+            count = count + 1
+    return count
+
+    return
+
 
 # Part B. is_power_of
 # Define a function is_power_of(i,j) that takes 2 ints i and j
 # and checks if i is a power of j or not
 # the function should return True indicating that i is a power of j
 # otherwise return False
-def is_power_of(i,j):
-  # YOUR CODE HERE
+def is_power_of(i, j):
+    if j == 1:
+        return True
+    if j == 0:
+        return i == 0
+    while j % i == 0:
+        j //= i
+    return j == 1
+    return
 
-  return
+    return
+
 
 # Part C. longest_word
 # Define a function longest_word(s) that takes a string s
@@ -29,6 +43,14 @@ def is_power_of(i,j):
 # if 2 or more words are tied as longest then return the one that occurs LAST in the sentence
 # if s is an empty string return an empty string
 def longest_word(s):
-  # YOUR CODE HERE
+    empty = ""
+    newString = s.split()
+    if (len(s) == 0):
+        return ""
+    else:
+        for words in newString:
+            if (len(words) >= len(empty)):
+                empty = words
+    return empty
 
-  return
+    return
