f23b4af2eac49caac964c2b83454054e8e2a777b
Harsh Patel <hpate105@uncc.edu>
2021-04-06 20:30:20 -0400

    flask app #4 - db queries

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -7,6 +7,8 @@ from flask import render_template
 from flask import request
 from flask import redirect, url_for
 from database import db
+from models import Note as Note
+from models import User as User
 
 app = Flask(__name__)  # create an app
 notes = {1: {'title': 'first note', 'text': 'This is my first note', 'date': '10-1-2020'},
@@ -26,27 +28,27 @@ with app.app_context():
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Harsh', 'email': 'hpate105@uncc.edu'}
+    a_user =  db.session.query(User).filter_by(email='hpate105@uncc.edu')
     return render_template('index.html', user=a_user)
 
 
 @app.route('/notes')
 def get_notes():
-
-    a_user = {'name': 'Harsh', 'email': 'hpate105@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='hpate105@uncc.edu')
+    my_notes = db.session.query(Note).all()
     return render_template('notes.html', notes=notes, user=a_user)
 
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-
-    a_user = {'name': 'Harsh', 'email': 'hpate105@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='hpate105@uncc.edu')
+    my_note = db.session.query(Note).filter_by(id=note_id)
     return render_template('note.html', note=notes[int(note_id)], user=a_user)
 
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_route():
-    a_user = {'name': 'Harsh', 'email': 'hpate105@uncc.edu'}
+    a_user = {'name': 'Harsh', 'email': 'hpate227@uncc.edu'}
     print('request method is', request.method)
     if request.method == 'POST':
         title = request.form['title']
@@ -54,10 +56,11 @@ def new_route():
         from datetime import date
         today = date.today()
         today = today.strftime("%m-%d-%Y")
-        id = len(notes)+1
-        notes[id] = {'title': title, 'text': text, 'date': today}
-        return redirect(url_for('get_notes', name = a_user))
+        newEntry = Note(title, text, today)
+        db.session.add(newEntry)
+        db.session.commit()
     else:
+        a_user = db.session.query(User).filter_by(email='hpate105@uncc.edu')
         return render_template('new.html', user=a_user)
 
 
