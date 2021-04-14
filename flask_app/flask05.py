461c226e13a2b3c2be96405f88fc9496a3c8f6ce
Harsh Patel <hpate105@uncc.edu>
2021-04-14 13:45:33 -0400

    flask app#5 - added route and function for edit

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -43,6 +43,13 @@ def get_note(note_id):
     my_note = db.session.query(Note).filter_by(id=note_id).one()
     return render_template('note.html', note=my_note, user=a_user)
 
+@app.route('/notes/edit/<note_id>')
+def update_note(note_id):
+    a_user = db.session.query(User).filter_by(email='hpate105@uncc.edu').one()
+    my_note = db.session.query(Note).filter_by(id=note_id).one()
+    return render_template('new.html', note=my_note, user=a_user)
+
+
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_route():
